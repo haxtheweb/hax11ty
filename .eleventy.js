7be25bd0847758faeca11db7da4eecce2cc98fdc
heymp <mgp140@gmail.com>
2020-04-19 20:48:08 -0400

    init

---
+++ .eleventy.js
@@ -0,0 +1,29 @@
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("assets");
+  eleventyConfig.setTemplateFormats(["html", "md"]);
+  eleventyConfig.addCollection("haxcms", function (collection) {
+    // const items = collection.items.map((i) => Object.keys(i))
+    const items = collection.items.map(({ outputPath, url, data }) => {
+      return {
+        id: outputPath.split(',').join('-'),
+        location: outputPath,
+        title: data.title,
+        parent: null
+      };
+    });
+    return { 
+      id: "3474a06d-9d3b-4ec7-ba9b-0a448a6e685e",
+      title: "Site Title",
+      author: "author",
+      description: "",
+      license: "MIT",
+      metadata: {
+        siteName: "Site Title",
+        theme: {
+          element: ""
+        }
+      },
+      items
+    };
+  });
+};
---
+++ posts/post1.md
@@ -0,0 +1,5 @@
+---
+title: Post 1
+---
+
+This is somthing cool!
