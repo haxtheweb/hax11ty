7f548e91c170b9d287c2c29a4899be9113d960a5
heymp <mgp140@gmail.com>
2020-04-21 12:28:24 -0400

    added eleventy-hax

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,3 +2,4 @@ import "./components/haxtheme-item.js";
 import "./components/haxtheme-banner.js";
 import "./components/theme-toggle.js";
 import "./components/haxtheme-hax-content.js";
+import "./components/eleventy-hax.js";
