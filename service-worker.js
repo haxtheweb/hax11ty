d48c467426137f11314a9169f4c0af8852368b5a
btopro <bto108@psu.edu>
2021-04-15 17:51:24 +0000

    Deploying to gh-pages from  @ ff3820820478cacaf5ca833d66450a38c8f88301 🚀

--- service-worker.js
+++ service-worker.js
@@ -38,20 +38,20 @@
 'use strict';
 var precacheConfig = [
   [
+    "/hax11ty/content/1_about/",
+    "vJydR0TnmknCGAvN"
+  ],
+  [
     "/hax11ty/content/2_install/",
-    "wP0qsJ1Y9HBCYFEA"
+    "vJydR0TnmknCGAvN"
   ],
   [
     "/hax11ty/content/3_why/",
-    "wP0qsJ1Y9HBCYFEA"
-  ],
-  [
-    "/hax11ty/content/1_about/",
-    "wP0qsJ1Y9HBCYFEA"
+    "vJydR0TnmknCGAvN"
   ],
   [
     "/hax11ty/",
-    "wP0qsJ1Y9HBCYFEA"
+    "vJydR0TnmknCGAvN"
   ]
 ];
 var cacheName = 'sw-precache-v3--' + (self.registration ? self.registration.scope : '');
